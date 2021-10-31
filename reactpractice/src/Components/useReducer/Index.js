8993718baff97f64a38a8009618610ef143e99c4
Anurag Yadav <anuragy554@gmail.com>
2021-10-31 19:30:49 +0530

    Added people to state using use reducer

--- reactpractice/src/Components/useReducer/Index.js
+++ reactpractice/src/Components/useReducer/Index.js
@@ -3,24 +3,33 @@ import { data } from '../../data'
 import Modal from './Modal'
 import { v4 as uuidv4 } from 'uuid';
 const reducer=(state,action)=>{
+if(action.type==="ADD_PEOPLE"){
+    const newPeople=[...state.peoples,action.payload]
+    return {...state,peoples:newPeople,isModalOpen:true,modalContent:"Added"}
 
 }
+if(action.type==="NO_VALUE"){
+    return {...state,isModalOpen:true,modalContent:"Cannot Be Added"}
+}
+throw new Error("No matching action Type Found")
+}
 const defaultState={
-    peoples:data,
-    isModalOpen:true,
+    peoples:[],
+    isModalOpen:false,
     modalContent:'Hello There First'
 }
 function Index() {
-    // const [showModal, setShowModal ]= useState(false)
-    // const [peoples, setPeoples ]= useState(data)
     const [state,dispatch]=useReducer(reducer,defaultState )
     const [name, setName]= useState("")
 const submitHandler=(e)=>{
     e.preventDefault()
     if(name){
-     
+        const newPeople={id:uuidv4(),name:name,}
+     dispatch({type:'ADD_PEOPLE',payload:newPeople})
     }
     else{
+        dispatch({type:'NO_VALUE'})
+
     }
 }
     return (
